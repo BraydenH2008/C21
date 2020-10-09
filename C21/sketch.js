31d8729e73a1cff4f71faf8aba5980a81f78a55f
BraydenH2008 <71739661+BraydenH2008@users.noreply.github.com>
2020-10-08 20:44:25 -0400

    Add files via upload

---
+++ C21/sketch.js
@@ -0,0 +1,41 @@
+var fixedRect, movingRect;
+var gameObject1,gameObject2,gameObject3,gameObject4;
+
+function setup() {
+  createCanvas(1200,800);
+  fixedRect = createSprite(600, 400, 50, 80);
+  fixedRect.shapeColor = "green";
+  fixedRect.debug = true;
+  
+  movingRect = createSprite(400,200,80,30);
+  movingRect.shapeColor = "green";
+  movingRect.debug = true;
+
+  gameObject1 = createSprite(1000, 200, 50, 80)
+  gameObject2 = createSprite(800, 600, 50, 80)
+  gameObject3 = createSprite(600, 750, 50, 80)
+  gameObject4 = createSprite(400, 100, 50, 80)
+  gameObject1.shapeColor = "green";
+  gameObject2.shapeColor = "green";
+  gameObject3.shapeColor = "green";
+  gameObject4.shapeColor = "green";
+
+}
+
+function draw() {
+  background(0,0,0);  
+  movingRect.x = World.mouseX;
+  movingRect.y = World.mouseY;
+
+  if(isTouching(movingRect,gameObject2)){
+    movingRect.shapeColor = "red";
+    gameObject2.shapeColor = "red";
+  }
+  else{
+    movingRect.shapeColor = "green";
+    gameObject2.shapeColor = "green";
+  }
+  
+  drawSprites();
+}
+
